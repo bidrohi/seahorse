a19a10d2f39da9522313783d4beeea07d1136825
Saud Khan <dev@bidyut.com>
2024-05-25 10:56:41 +0800

    Clean up JsonFallbackSource

--- examples/jvm_desktop/src/commonMain/kotlin/com/bidyut/tech/seahorse/data/JsonResourceFallbackSource.kt
+++ examples/jvm_desktop/src/commonMain/kotlin/com/bidyut/tech/seahorse/data/JsonResourceFallbackSource.kt
@@ -4,20 +4,14 @@ import com.bidyut.tech.seahorse.jvm_desktop.generated.resources.Res
 import com.bidyut.tech.seahorse.model.LanguageId
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
-import kotlinx.serialization.json.Json
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 
 @OptIn(ExperimentalResourceApi::class)
 class JsonResourceFallbackSource : JsonFallbackSource() {
-    override fun fetchStrings(languageId: LanguageId): Map<String, String> {
+    override fun readStrings(languageId: LanguageId): Map<String, String> {
         return runBlocking(Dispatchers.IO) {
             val bytes = Res.readBytes("files/$languageId.json")
-            val json = Json {
-                ignoreUnknownKeys = true
-                isLenient = true
-            }
-            val stringMap = json.decodeFromString<Map<String, String>>(bytes.decodeToString())
-            stringMap
+            parseStrings(bytes.decodeToString())
         }
     }
 }
--- seahorse/core/src/commonMain/kotlin/com/bidyut/tech/seahorse/data/JsonFallbackSource.kt
+++ seahorse/core/src/commonMain/kotlin/com/bidyut/tech/seahorse/data/JsonFallbackSource.kt
@@ -3,8 +3,15 @@ package com.bidyut.tech.seahorse.data
 import com.bidyut.tech.seahorse.model.LanguageEnglish
 import com.bidyut.tech.seahorse.model.LanguageId
 import com.bidyut.tech.seahorse.utils.formatString
+import com.bidyut.tech.seahorse.utils.sanitiseFormatString
+import kotlinx.serialization.json.Json
 
-abstract class JsonFallbackSource : FallbackSource {
+abstract class JsonFallbackSource(
+    protected val json: Json = Json {
+        ignoreUnknownKeys = true
+        isLenient = true
+    },
+) : FallbackSource {
     private var languageId: LanguageId = LanguageEnglish
     private val stringMapByLanguage = mutableMapOf<LanguageId, Map<String, String>>()
 
@@ -12,7 +19,9 @@ abstract class JsonFallbackSource : FallbackSource {
         languageId: LanguageId,
     ) {
         if (!stringMapByLanguage.contains(languageId)) {
-            stringMapByLanguage[languageId] = fetchStrings(languageId)
+            stringMapByLanguage[languageId] = readStrings(languageId).mapValues {
+                sanitiseFormatString(it.value)
+            }
         }
         this.languageId = languageId
     }
@@ -24,7 +33,11 @@ abstract class JsonFallbackSource : FallbackSource {
         formatString(it, *formatArgs)
     }
 
-    abstract fun fetchStrings(
+    protected fun parseStrings(
+        jsonBody: String,
+    ): Map<String, String> = json.decodeFromString<Map<String, String>>(jsonBody)
+
+    abstract fun readStrings(
         languageId: LanguageId,
     ): Map<String, String>
 }
